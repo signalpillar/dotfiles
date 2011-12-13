f01d675bd159936341784283c8cebfdb7b5ee3b1
signalpillar <signalpillar@github>
2011-12-13 21:34:44 +0200

    emacs24:scala support

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -8,3 +8,17 @@
 
 ;; allow selection deletion
 (delete-selection-mode t)
+
+;;; {{{ Scala mode
+(add-to-list 'load-path "~/.emacs.d/scala-mode")
+(require 'scala-mode-auto)
+(add-to-list 'auto-mode-alist '("\\.scala\\'" . scala-mode))
+;;; }}}
+;;; {{{ Ensime
+(add-to-list 'load-path "~/.emacs.d/ensime/elisp")
+(require 'ensime)
+
+;; free of trailing whitespace and to use 80-column width, standard indentation
+(setq whitespace-style '(trailing lines space-before-tab
+                                  indentation space-after-tab)
+      whitespace-line-column 80)
