f1fb0126d0afffead10120206a44d6272a1c4ada
vvitvitskiy <vvitvitskiy@dd-rnd-vv.(none)>
2011-12-20 13:41:06 +0200

    emacs24: auto-complete mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -16,3 +16,9 @@
 
 
 (hl-line-mode)
+
+;; auto-complete configuration
+(add-to-list 'load-path "~/.emacs.d/auto-complete/")
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/auto-complete//ac-dict")
+(ac-config-default)
