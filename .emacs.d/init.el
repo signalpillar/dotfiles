0bdd5fdaddfc61669a9986972a4fff4ee0f39baf
signalpillar <signalpillar@signalpillar.(none)>
2012-01-09 23:01:38 +0200

    emacs: updated config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -22,3 +22,25 @@
 (require 'auto-complete-config)
 (add-to-list 'ac-dictionary-directories "~/.emacs.d/auto-complete//ac-dict")
 (ac-config-default)
+
+;; scala-mode
+(require 'scala-mode)
+(add-hook 'scala-mode-hook
+          '(lambda ()
+             (yas/minor-mode-on)
+             ))
+
+;; enable color theme solarized
+(color-theme-solarized-light)
+
+
+;; recent mode
+(recentf-mode 1)
+(global-set-key "\C-cf" 'recentf-open-files)
+
+;; enable menu bar
+(menu-bar-mode 1)
+;; show column number along with row
+(column-number-mode t)
+;; font configuration
+(set-default-font "DejaVu Sans Mono-11")
