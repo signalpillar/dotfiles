bd1cd2bf24483c386e059e11abd3e96fe201c932
vvitvitskiy <vvitvitskiy@vvitvitskiy-at-work.(none)>
2012-02-13 11:23:33 +0200

    run gnome setting daemon in openbox

--- .config/openbox/autostart.sh
+++ .config/openbox/autostart.sh
@@ -7,5 +7,5 @@ nm-applet &
 xmodmap ~/.xmodmap &
 setxkbmap -layout "us,ua,ru" -option "grp:alt_shift_toggle" &
 feh --bg-tile ~/.config/openbox/wall/ubuntubokehfull1280x960.png &
-
+gnome-settings-daemon &
 
