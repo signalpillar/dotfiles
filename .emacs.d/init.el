7ea8925fe7bb940a9a9d2125072012d6732d7cad
signalpillar <signalpillar@signalpillar>
2012-10-08 00:14:24 +0300

    #emacs get rid of starter-kit

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,256 +1,133 @@
-;; Turn off mouse interface early in startup to avoid momentary display
-(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
-(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
-(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+(setq-default inhibit-startup-screen t)
+;; update "PATH" variable that is used from non-login shell
+(let (
+      (mypaths
+       (list 
+         "/home/spillar/bin"
+         (getenv "PATH")
+         )
+       ))
 
-;; run as server
-(require 'server)
-(unless (server-running-p)
-  (server-start))
+  (setenv "PATH" (mapconcat 'identity mypaths ":") )
+  (setq exec-path (append mypaths (list "." exec-directory)) )) 
 
-; Prevent noise when C-g is hit
-(setq visible-bell t)
-(blink-cursor-mode)
-(show-paren-mode t)
-                                        ; package managment
 (require 'package)
+;; declare repositories that will be used by package.el
 (add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+	     '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+	     '("melpa" . "http://melpa.milkbox.net/packages/"))
+(add-to-list 'package-archives
+	     '("gnu" . "http://elpa.gnu.org/packages/"))
+
+;; load packages installed with package manager
 (package-initialize)
-
-                                        ; ELSCREEN
-(add-to-list 'load-path "~/.emacs.d/escreen/")
-(load "escreen")
-(escreen-install)
-(setq escreen-prefix-char "\C-a") ;; http://www.macs.hw.ac.uk/~hwloidl/cool-el.html
-(global-set-key escreen-prefix-char 'escreen-prefix)
-;; add C-\ l to list screens with emphase for current one
-
-                                        ; EDITOR CONFIGURATION
-; highlight entire expression
-(setq show-paren-style 'expression)
-; disable wordwrap by default
-(setq-default truncate-lines nil)
-(setq lisp-indent-offset 2)
-;; Use only spaces (no tabs at all).
-(setq-default indent-tabs-mode nil)
-;; Always show column numbers.
-(setq-default column-number-mode t)
-;; Display full pathname for files.
-(add-hook 'find-file-hooks
-          '(lambda ()
-             (setq mode-line-buffer-identification 'buffer-file-truename)))
-
-(when (not package-archive-contents)
-  (package-refresh-contents))
-
 ;; Add in your own as you wish:
-(defvar my-packages '(starter-kit starter-kit-lisp starter-kit-bindings)
+(defvar my-packages '(evil ac-slime slime-fuzzy
+			   fill-column-indicator
+			   ;clojure related
+			   clojure-mode clojure-test-mode nrepl
+			   )
   "A list of packages to ensure are installed at launch.")
+; gui
+(tool-bar-mode 0)
 
-(dolist (p my-packages)
-  (when (not (package-installed-p p))
-    (package-install p)))
-
-;; enable evil-mode
+; global editor settings
+(setq-default indent-tabs-mode nil)        ; use only spaces (no tabs at all)
+(set-frame-font "Monaco-10")               ; change font
+(load-theme 'tango-dark)
+(column-number-mode t)
+(size-indication-mode t)                   ; show file size
+;; show right margin (80symb)
+(define-globalized-minor-mode global-fci-mode fci-mode
+  (lambda () (fci-mode 1)))
+(global-fci-mode 1)
+(setq-default fill-column 79)
+; show line numbers always
+(global-linum-mode 1)
+; show paren mode
+(show-paren-mode 1)
+(setq show-paren-style 'expression)
+(set-face-background 'show-paren-match-face "#4F5152")
+;;; enable evil-mode
 (require 'evil)
 (evil-mode 1)
 
-(defun turn-on-paredit () (paredit-mode 1))
-
-;; -----------------------------------------------------
-;; draw fill-column line
-; (add-to-list 'load-path "/path/to/highlight-80+")
-; (require 'highlight-80+)
-;; -----------------------------------------------------
-
-;; clojure-mode
-(add-hook 'clojure-mode-hook
-          (lambda ()
-            (turn-on-paredit)
-            (turn-on-eldoc-mode)
-            (show-paren-mode t)
-            (set-up-slime-ac)
-            (fci-mode)))
-
-;; clojure-script mode
-(add-to-list 'auto-mode-alist '("\.cljs$" . clojure-mode))
-
-
-;; render line numbers
-(global-linum-mode)
-
-;; multi-scratch
-(add-to-list 'load-path "~/.emacs.d/misc")
-(require 'multi-scratch)
-(global-set-key (kbd "C-<f11>") 'multi-scratch-new)
-
-(load-theme 'tsdh-dark)
-
-;; auto-complete
+;; autocomplete
 (require 'auto-complete-config)
+;; show menu after 0.5 seconds
+(setq ac-auto-show-menu 0.5)
+(setq ac-show-menu-immediately-on-auto-complete t)
+(setq ac-use-fuzzy t)
+;; ignore case
+(setq ac-ignore-case t)
 (ac-config-default)
-
-(require 'ac-slime)
+;; ac-slime
 (add-hook 'slime-mode-hook 'set-up-slime-ac)
 (add-hook 'slime-repl-mode-hook 'set-up-slime-ac)
 (eval-after-load "auto-complete"
   '(add-to-list 'ac-modes 'slime-repl-mode))
 
-
-;; comment-or-uncomment
-;; (global-set-key (kbd "C-S /") 'comment-or-uncomment-region)
-
-
-(set-default 'indicate-empty-lines t)
-
-(defun cleanup-buffer-safe ()
-  "Perform a bunch of safe operations on the whitespace content of a buffer.
-Does not indent buffer, because it is used for a before-save-hook, and that
-might be bad."
-  (interactive)
-  (untabify-buffer)
-  (delete-trailing-whitespace)
-  (set-buffer-file-coding-system 'utf-8))
-
-(defun cleanup-buffer ()
-  "Perform a bunch of operations on the whitespace content of a buffer.
-Including indent-buffer, which should not be called automatically on save."
-  (interactive)
-  (cleanup-buffer-safe)
-  (indent-buffer))
-
-(global-set-key (kbd "C-c n") 'cleanup-buffer)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(blink-cursor-mode nil)
- '(column-number-mode t)
- '(custom-safe-themes (quote ("1f2b1f771fb60d7fb0f31d7adfc1e1c4524c7318c3aeb100ad6fab4dce389c0b" "71efabb175ea1cf5c9768f10dad62bb2606f41d110152f4ace675325d28df8bd" default)))
- '(show-paren-mode t)
- '(tool-bar-mode nil))
-
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:family "DejaVu Sans Mono" :foundry "unknown" :slant normal :weight normal :height 98 :width normal)))))
-
-
-                                        ; eproject
-(add-to-list 'load-path "~/.emacs.d/eproject")
-(require 'eproject)
-(require 'eproject-extras)
-                                        ; projects type configuration
-
-(define-project-type clojure (generic)
-  (look-for "project.clj")
-  :relevant-files ("\.clj$"))
-
-(define-project-type scala (generic)
-  (look-for ".scalaproject")
-  :relevant-files ("\.java$" "\.scala$"))
-
-(define-project-type pydevproject (generic)
-  (look-for ".pydevproject")
-  :relevant-files ("\.py$" "\.xml$" "\.md$" "\.properties$"))
-
-
-                                        ; jython stuff
-
-(defun compose-cmd-line (jython-home python-path python-ext-path script)
-  "Compose command line for jython script execution")
-
-(defun file-read-content (file)
-  "Read the contents of a file and return as a string."
-  (with-temp-buffer
-    (insert-file-contents file)
-    (buffer-string)))
-
-(defun file-write-content (file content)
-   (with-temp-buffer
-     (insert content)
-     (when (file-writable-p file)
-       (write-region (point-min)
-                     (point-max)
-                     file))))
-
-(defun parse-pydevproject (content))
-
-(defun jj-run-jython ()
-  "Execute jython tests in currect buffer"
-  (interactive)
-  ; take project root folder and read .pydevproject file 
-  (let ((project-file (concat (eproject-root) "/.pydevproject"))
-         (run-file (concat (eproject-root) "/.pydevrun"))
-         (one-hour-in-sec (* 60 60)))
-    ; check for the stored file .pydevrun
-    (when (or (not (file-exists-p run-file))
-            (progn (let
-                     (attrs (file-attributes run-file))
-                     (modification-time (float-time (elt attrs 5)))
-                     (current-time (float-time))
-                     ; compute difference 1 HOUR
-                     (> (- current-time modification-time) one-hour-in-sec))))
       
-      ; get content of .pydevproject file
-      (setq jython-home "/media/repos/dt/jython-2.1")
-      (setq pydevproject (parse-pydevproject (file-read-content project-file)))
-      (file-write-content run-file
-        (compose-cmd-line
-          (string jython-home "/jython.jar")
-          ; pythonpath and python ext path (libs)
-          (elt pydevproject 0) (elt pydevproject 1))))
+;; paredit
+(autoload 'paredit-mode "paredit" t)
+(add-hook 'emacs-lisp-mode-hook (lambda () (paredit-mode +1)))
+(add-hook 'slime-repl-mode-hook (lambda () (paredit-mode +1)))
+(add-hook 'lisp-mode-hook (lambda () (paredit-mode +1)))
+(add-hook 'lisp-interaction-mode-hook (lambda () (paredit-mode +1)))
 
-    ; execute & redirect output to temporary buffer
-    (show-in-temporary-buffer
-      (execute-shell-cmd
-        (string (get-file-content run-file) (buffer-file-name))))))
+;; eldoc
+(require 'eldoc)
+(eldoc-add-command
+ 'paredit-backward-delete
+ 'paredit-close-round)
 
-                                        ; make anythin.el to work with eproject
-(require 'cl)
-(defun anything-eproject-get-files ()
-  (let ((matcher (format "\\(?:%s\\)"
-                         (reduce (lambda (a b) (concat a "\\|" b))
-                                 (mapcar (lambda (f) (format "\\(?:%s\\)" f))
-                                         (eproject-get-project-metadatum
-                                          (eproject-type) :relevant-files))))))
-    (eproject--search-directory-tree (eproject-root) matcher "$^")))
+;;ido
+(custom-set-variables
+ '(ido-enable-last-directory-history t)
+ '(ido-record-commands t)
+ '(ido-max-work-file-list 10)
+ '(ido-max-work-directory-list 5))
 
-(defvar anything-eproject-source
-  '((name . "Eproject")
-    (init . (lambda ()
-              (setq anything-eproject-last-buffer (current-buffer))))
-    (type . file)
-    (candidates . (lambda ()
-                    (with-current-buffer anything-eproject-last-buffer
-                      (anything-eproject-get-files))))))
+(setq ido-enable-flex-matching t)
+(setq ido-everywhere t)
+(ido-mode 1)
 
-(defun any-eproject ()
-  "helps anything to use eproject to find a file"
-  (interactive)
-  (let ((anything-sources '(anything-eproject-source)) anything-samewindow)
-    (anything nil nil nil nil nil "eproject")))
-
-
-(global-set-key (kbd "M-p") 'any-eproject)
-;; (global-set-key (kbd "M-S-<up>") 'enlarge-window)
-;; (global-set-key (kbd "M-S-<down>") 'shrink-window)
-(global-set-key (kbd "<f8>") (lambda () (interactive) (message (buffer-file-name))))
-
-
-                                        ; nrepl
-
+;; nrepl
+;; enable eldoc in clojure buffers
 (add-hook 'nrepl-interaction-mode-hook
-  'nrepl-turn-on-eldoc-mode)
+	  'nrepl-turn-on-eldoc-mode)
+;; stop the error buffer from popping up while working in the REPL
+(setq nrepl-popup-stacktraces nil)
 
-(add-to-list 'same-window-buffer-names "*nrepl")
 
-;; Fill column indicator
-(require 'fill-column-indicator)
-(setq fci-rule-color "#111122")
+;; recentf
+(recentf-mode 1)
+(setq recentf-max-saved-items 500)
+(setq recentf-max-menu-items 60)
+(global-set-key [(meta f12)] 'recentf-open-files)
+
+;; ibuffer configuration
+(setq ibuffer-shrink-to-minimum-size t)
+(setq ibuffer-always-show-last-buffer nil)
+(setq ibuffer-sorting-mode 'recency)
+(setq ibuffer-use-header-line t)
+(global-set-key [(f12)] 'ibuffer)
+
+; clojure 
+(setq inferior-lisp-program "lein repl")
+(add-hook 'clojure-mode-hook
+          (lambda ()
+            ((turn-on-paredit)
+             )))
+
+; elscreen
+(load "elscreen" "ElScreen" t)
+(setq elscreen-prefix-key (kbd "C-c c")
+      elscreen-display-tab nil)
+(global-unset-key elscreen-prefix-key)
+(global-set-key elscreen-prefix-key elscreen-map)
+(elscreen-start)
+
+;; used resources
+; http://www.xsteve.at/prg/emacs/power-user-tips.html
