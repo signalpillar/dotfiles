d8c74e34bd79cbbe9aedd860f6f24079fc603ec9
signalpillar <signalpillar@signalpillar>
2013-04-09 22:07:35 +0300

    clojure: new plugin simpleton

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -2,6 +2,7 @@
  {
   :local-repo "/media/repos/.m2/repository/"
   :plugins [
+            [lein-simpleton "1.0.0"]
             [lein-iclojure "1.1"]
             [lein-exec "0.3.0"]
             [lein-swank "1.4.4"]
