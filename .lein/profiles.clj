8c8e5f50864e63db5b1b5f381bf1bbb21226d1d7
signalpillar <signalpillar@signalpillar>
2013-04-09 10:02:22 +0300

    clojre: iclojure plugin enabled and local-repo path specified

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,4 +1,8 @@
-{:user {:plugins [
-                  [lein-exec "0.3.0"]
-                  [lein-swank "1.4.4"]
-                  [lein-marginalia "0.7.1"]]}}
+{:user
+ {
+  :local-repo "/media/repos/.m2/repository/"
+  :plugins [
+            [lein-iclojure "1.1"]
+            [lein-exec "0.3.0"]
+            [lein-swank "1.4.4"]
+            [lein-marginalia "0.7.1"]]}}
