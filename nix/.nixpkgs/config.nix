3952167e90ed1a5a3065771c31cb5f0681afa5b2
Volodymyr Vitvitskyi <contact.volodymyr@gmail.com>
2019-11-09 21:28:38 +0000

    [nix] Add lorri to my configuration

--- nix/.nixpkgs/config.nix
+++ nix/.nixpkgs/config.nix
@@ -2,7 +2,21 @@
   # You won't be able to install or search for an unfree package as a user,
   # unless you explicitly enable.
   allowUnfree = true;
+
   packageOverrides = pkgs: with pkgs; rec {
+
+    # nix-shell -p lorri ( to test build )
+    lorri = (
+      let
+        src = pkgs.fetchFromGitHub {
+              owner  = "target";
+              repo   = "lorri";
+              rev    = "03f10395943449b1fc5026d3386ab8c94c520ee3";
+              sha256 = "0fcl79ndaziwd8d74mk1lsijz34p2inn64b4b4am3wsyk184brzq";
+        };
+      in pkgs.callPackage src { inherit src; }
+    );
+
     osx = buildEnv {
       name = "osx-desktop";
       paths = [
@@ -18,6 +32,7 @@
         gdb
         gitAndTools.gitFull
         jq
+        lorri
         # used for emacs-terminal integration
         # osx: not supported
         # libvterm
