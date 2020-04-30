8c82547a8e748e43af3017f3103470aaadc05870
Volodymyr Vitvitskyi <contact.volodymyr@gmail.com>
2020-04-30 22:10:53 +0100

    [kitty] track config changes and theme

--- run_once_0_kitty.sh
+++
@@ -1,5 +0,0 @@
-# We need to run this to let tmux know what is kitty terminal
-KITTY_TERMINFO_DIR=/Applications/kitty.app/Contents/Frameworks/kitty/terminfo
-if [ -d $KITTY_TERMINFO_DIR ]; then
-    ln -Fs $KITTY_TERMINFO_DIR  ~/.terminfo
-fi
---
+++ run_once_99_kitty.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# We need to run this to let tmux know what is kitty terminal
+KITTY_TERMINFO_DIR=/Applications/kitty.app/Contents/Frameworks/kitty/terminfo
+if [ -d $KITTY_TERMINFO_DIR ]; then
+    ln -Fs $KITTY_TERMINFO_DIR  ~/.terminfo
+fi
+
+
+THEMES_DIR=~/.config/kitty/kitty-themes
+if [ ! -d $THEMES_DIR ]; then
+    git clone --depth 1 git@github.com:dexpota/kitty-themes.git $THEMES_DIR
+fi
+
+
+ln -Fs $THEMES_DIR/themes/AtomOneLight.conf ~/.config/kitty/theme.conf
+
+
