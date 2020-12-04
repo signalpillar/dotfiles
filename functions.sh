f4ccee61ff922113ca526650bcebf3077efa1c76
Volodymyr Vitvitskyi <volodymyr.vitvitskyi@B-0309.local>
2020-12-04 11:12:51 +0000

    Add ranger config

--- functions.sh
+++ functions.sh
@@ -394,3 +394,16 @@ function debug-http-server {
     echo "Start debug server on port '$port' that will respond with 200 status code"
     for i in `seq 5`; do { echo -e "HTTP/1.1 200 OK\r\n\n{\"nc_response_number\": $i}";  } | nc -l 8004; done
 }
+
+
+function devider {
+    # https://iterm2.com/documentation-images.html
+    if [ $# -eq 0 ]; then
+        echo "Usage: divider file"
+        exit 1
+    fi
+    printf '\033]1337;File=inline=1;width=100%%;height=1;preserveAspectRatio=0'
+    printf ":"
+    base64 < "$1"
+    printf '\a\n'
+}
