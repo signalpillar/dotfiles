8c7de99129a76f0c0b4ce3794e8660b110dfd3d5
Volodymyr Vitvitskyi <volodymyr.vitvitskyi@babylonhealth.com>
2022-11-21 12:42:41 +0000

    [nixos] Another attempt to use nixos in the vm

---
+++ nix/vm2.configuration.nix
@@ -0,0 +1,535 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, lib, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+       # <nixpkgs/nixos/modules/virtualisation/virtualbox-image.nix>
+    ];
+
+  # VIRTUALISATION
+  virtualisation.docker = {
+    enable = true;
+    autoPrune.enable = true;
+  };
+  virtualisation.vmware.guest.enable = true;
+
+  # Bootloader.
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/sda";
+  boot.loader.grub.useOSProber = true;
+
+
+  networking.hostName = "B-5014";
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  environment.variables = {
+    EDITOR = "vim";
+    # LANG = "en_US.UTF-8";
+    # LC_ALL = "en_US.UTF-8";
+    # source: http://itchyknowsdevs.me/blog/developing-golang-in-nixos/
+    GOROOT = [ "${pkgs.go.out}/share/go" ];
+    TERMINAL = "kitty";
+    RIPGREP_CONFIG_PATH = "/home/demo/.ripgreprc";
+  };
+
+  # the link is required for i3
+  # see https://nixos.wiki/wiki/I3
+  environment.pathsToLink = [ 
+    "/libexec" 
+    "/usr/bin"
+  ];
+
+  environment.sessionVariables = rec {
+    PATH = [ 
+      "$HOME/bin"
+      "$HOME/.rd/bin"
+      "$HOME/.npm/bin"
+      "$HOME/mutable_node_modules/bin"
+    ];
+  };
+
+  # Set your time zone.
+  time.timeZone = "Europe/London";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_GB.utf8";
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+  services.xserver.videoDrivers = lib.mkOverride 40 [ "vmware" "cirrus" "vesa" "modesetting"];
+
+  # Enable the XFCE Desktop Environment.
+  services.xserver.displayManager.lightdm.enable = true;
+  # services.xserver.desktopManager.xfce.enable = true;
+
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "us";
+    xkbVariant = "";
+    autoRepeatDelay = 200;
+
+    windowManager.i3 = {
+        enable = true;
+        package = pkgs.i3-gaps;
+        extraPackages = with pkgs; [
+            i3-gaps
+            i3status # gives you the default i3 status bar
+            i3lock #default i3 screen locker
+            rofi  # application launcher
+         ];
+      };
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.demo = {
+    isNormalUser = true;
+    description = "demo";
+    shell = pkgs.zsh;
+    extraGroups = [ "networkmanager" "wheel" "sudo" "docker" "vboxsf" "dialout" "plugdev" "adbusers"];
+    packages = with pkgs; [
+    #  firefox
+    #  thunderbird
+    ];
+  };
+
+  nix = {
+    autoOptimiseStore = true;
+    gc.automatic = false;
+    optimise.automatic = true;
+  };
+
+  documentation.man.enable = true;
+
+  environment.shellAliases = {
+    # Use emacsclient to open files in current emacs instance server
+    ec = "emacsclient -nw";
+    ecw = "emacsclient -cn";
+
+    nixre = "darwin-rebuild switch";
+    nixgc = "nix-collect-garbage -d";
+
+    ll = "ls -althF";
+    la = "ls -A";
+    l = "ls -CF";
+    tmux = "tmux -2";
+    ddc = "docker-compose";
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+# nixpkgs.overlays = [
+#   (import (builtins.fetchTarball {
+#     url = https://github.com/nix-community/emacs-overlay/archive/master.tar.gz;
+#   }))
+# ];
+
+# List packages installed in system profile. To search, run:
+# \$ nix search wget
+environment.systemPackages = with pkgs; [
+  # TODO: https://github.com/multiprocessio/dsq
+  # search engines
+   ripgrep
+   # ....
+   _1password
+   chezmoi
+   cmake
+   direnv
+   mc
+   file
+   fzf
+   gcc
+   git
+   gnumake
+   htop
+   libffi
+   libnotify
+   lshw
+   mplayer
+   ncdu
+
+   android-udev-rules
+
+   pstree
+   ctop
+
+   nodejs
+
+   # openjdk
+   adoptopenjdk-bin
+
+   openssl
+   pkg-config
+   tmux
+   tmuxp
+   tree
+   unzip
+   zeal
+   zathura
+   xclip
+   which
+   wget
+
+   zsh
+   oh-my-zsh
+
+   gccStdenv
+
+   tldr  # community-driven man pages
+   bat   # clone of cat
+
+   stdenv.cc.cc.lib
+
+   # ---- Configure
+  # ARandR is designed to provide a simple visual front end for XRandR. Relative monitor positions are shown graphically and can be changed in a drag-and-drop way.
+   # arandr
+
+   # ---- Desktop
+   brave
+   firefox
+   feh
+   dropbox
+   parcellite
+
+   # ---- Terminals
+   kitty
+   any-nix-shell
+   fortune
+   nushell
+
+   # ---- Python
+  (python39.withPackages(ps: with ps; [
+      pip
+      black
+      isort
+  ]))
+
+   # ---- Docker & K8S
+   #  teleport version 4.2.11 is needed to make it work with shipcat
+   (import (builtins.fetchTarball {
+     name = "nixos-21.05";
+     url = "https://github.com/NixOS/nixpkgs/archive/2fed8df61dc5f7a0f728947706c5114c36f04497.tar.gz";
+     # Hash obtained using `nix-prefetch-url --unpack <url>`
+     sha256 = "0n979s0v9wfn21fl5q3g0p58l85c35s7ajvq3cmzv0i8l8lfyghj";
+   }) {}).teleport
+   kubectl
+   dive  # tool to explore each layer of the docker image
+   docker
+   docker-compose
+
+   # ---- Editors & Co
+   # https://github.com/errata-ai/vale
+   vale
+   aspell
+   aspellDicts.en
+   graphviz
+   global
+
+   emacs
+
+#  (emacs.emacsWithPackages (epkgs: [
+#    epkgs.vterm
+#    epkgs.pdf-tools
+#  ]))
+   (
+        pkgs.neovim.override {
+          vimAlias = true;
+          configure = {
+            packages.myPlugins = with pkgs.vimPlugins; {
+              start = [
+                vim-lastplace
+                vim-nix
+                nerdcommenter #preservim/nerdcommenter
+                vim-sleuth #tpope/vim-sleuth
+                vim-surround #tpope/vim-surround
+                vim-test #janko/vim-test
+              ];
+              opt = [];
+            };
+            # customRC = builtins.readFile ./../dotfiles/.vimrc;
+          };
+        }
+   )
+];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  programs.adb.enable = true;
+
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    # the paste is very slow with this option enabled
+    # autosuggestions.enable = true;
+    # https://github.com/nix-community/home-manager/issues/1226
+    ohMyZsh = {
+      enable = true;
+      customPkgs = [];
+      theme = "amuse";
+      # zsh-navigation-tools
+      plugins = [ "git" "tmux" "z" "docker" "colored-man-pages"];
+    };
+    interactiveShellInit = ''
+      source "$(fzf-share)/key-bindings.zsh"
+      source "$(fzf-share)/completion.zsh"
+      eval "$(direnv hook zsh)"
+      export DIRENV_LOG_FORMAT= # Silence direnv
+    '';
+    promptInit = ''
+      any-nix-shell zsh --info-right | source /dev/stdin
+    '';
+    syntaxHighlighting.enable = true;
+  };
+
+  fonts = {
+     fontDir.enable = true;
+     fontconfig.cache32Bit = true;
+     fonts = with pkgs; [
+        dejavu_fonts
+        fira-code
+        go-font
+        ibm-plex
+        inconsolata
+        proggyfonts
+        jetbrains-mono
+        emacs-all-the-icons-fonts
+     ];
+   };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # To check whether the daemon is running: systemctl --user list-units ssh-agent.service --all
+  # To check the logs: journalctl _SYSTEMD_UNIT=ssh-agent.service
+  programs.ssh.startAgent = true;
+
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "22.05"; # Did you read the comment?
+
+
+  environment.etc."i3.conf".text = ''
+# Please see http://i3wm.org/docs/userguide.html for a complete reference!
+
+# i3-msg restart
+# i3-msg reload
+
+# To see the key names
+# nix-shell -p xorg.xev --run "xev -event keyboard"
+
+# https://faq.i3wm.org/question/2569/set-mod-key-only-as-left-mod-key.1.html
+set $mod Mod1
+
+# Font for window titles. Will also be used by the bar unless a different font
+# is used in the bar {} block below. ISO 10646 = Unicode
+font -misc-fixed-medium-r-normal--13-120-75-75-C-70-iso10646-1
+# The font above is very space-efficient, that is, it looks good, sharp and
+# clear in small sizes. However, if you need a lot of unicode glyphs or
+# right-to-left text rendering, you should instead use pango for rendering and
+# chose a FreeType font, such as:
+# font pango:DejaVu Sans Mono 10
+
+# Use Mouse+$mod to drag floating windows to their wanted position
+floating_modifier $mod
+
+# start a terminal
+# bindsym $mod+Return exec i3-sensible-terminal
+bindsym $mod+Return exec kitty
+
+# kill focused window
+bindsym $mod+Shift+q kill
+
+# start dmenu (a program launcher)
+bindsym $mod+d exec rofi -show combi
+# There also is the (new) i3-dmenu-desktop which only displays applications
+# shipping a .desktop file. It is a wrapper around dmenu, so you need that
+# installed.
+# bindsym $mod+d exec --no-startup-id i3-dmenu-desktop
+
+# change focus
+bindsym $mod+j focus left
+bindsym $mod+k focus down
+bindsym $mod+l focus up
+bindsym $mod+semicolon focus right
+
+# alternatively, you can use the cursor keys:
+bindsym $mod+Left focus left
+bindsym $mod+Down focus down
+bindsym $mod+Up focus up
+bindsym $mod+Right focus right
+
+# move focused window
+bindsym $mod+Shift+j move left
+bindsym $mod+Shift+k move down
+bindsym $mod+Shift+l move up
+bindsym $mod+Shift+semicolon move right
+
+# alternatively, you can use the cursor keys:
+bindsym $mod+Shift+Left move left
+bindsym $mod+Shift+Down move down
+bindsym $mod+Shift+Up move up
+bindsym $mod+Shift+Right move right
+
+# split in horizontal orientation
+bindsym $mod+h split h
+
+# split in vertical orientation
+bindsym $mod+v split v
+
+# enter fullscreen mode for the focused container
+bindsym $mod+f fullscreen
+
+# change container layout (stacked, tabbed, toggle split)
+bindsym $mod+s layout stacking
+bindsym $mod+w layout tabbed
+bindsym $mod+e layout toggle split
+
+# toggle tiling / floating
+bindsym $mod+Shift+space floating toggle
+
+# change focus between tiling / floating windows
+bindsym $mod+space focus mode_toggle
+
+# focus the parent container
+bindsym $mod+a focus parent
+
+# focus the child container
+#bindsym $mod+d focus child
+
+# switch to workspace
+bindsym $mod+1 workspace 1
+bindsym $mod+2 workspace 2
+bindsym $mod+3 workspace 3
+bindsym $mod+4 workspace 4
+bindsym $mod+5 workspace 5
+bindsym $mod+6 workspace 6
+bindsym $mod+7 workspace 7
+bindsym $mod+8 workspace 8
+bindsym $mod+9 workspace 9
+bindsym $mod+0 workspace 10
+
+# move focused container to workspace
+bindsym $mod+Shift+1 move container to workspace 1
+bindsym $mod+Shift+2 move container to workspace 2
+bindsym $mod+Shift+3 move container to workspace 3
+bindsym $mod+Shift+4 move container to workspace 4
+bindsym $mod+Shift+5 move container to workspace 5
+bindsym $mod+Shift+6 move container to workspace 6
+bindsym $mod+Shift+7 move container to workspace 7
+bindsym $mod+Shift+8 move container to workspace 8
+bindsym $mod+Shift+9 move container to workspace 9
+bindsym $mod+Shift+0 move container to workspace 10
+
+# reload the configuration file
+bindsym $mod+Shift+c reload
+# restart i3 inplace (preserves your layout/session, can be used to upgrade i3)
+bindsym $mod+Shift+r restart
+# exit i3 (logs you out of your X session)
+bindsym $mod+Shift+e exec "i3-nagbar -t warning -m 'You pressed the exit shortcut. Do you really want to exit i3? This will end your X session.' -b 'Yes, exit i3' 'i3-msg exit'"
+
+# resize window (you can also use the mouse for that)
+mode "resize" {
+        # These bindings trigger as soon as you enter the resize mode
+
+        # Pressing left will shrink the window’s width.
+        # Pressing right will grow the window’s width.
+        # Pressing up will shrink the window’s height.
+        # Pressing down will grow the window’s height.
+        bindsym j resize shrink width 10 px or 10 ppt
+        bindsym k resize grow height 10 px or 10 ppt
+        bindsym l resize shrink height 10 px or 10 ppt
+        bindsym semicolon resize grow width 10 px or 10 ppt
+
+        # same bindings, but for the arrow keys
+        bindsym Left resize shrink width 10 px or 10 ppt
+        bindsym Down resize grow height 10 px or 10 ppt
+        bindsym Up resize shrink height 10 px or 10 ppt
+        bindsym Right resize grow width 10 px or 10 ppt
+
+        # back to normal: Enter or Escape
+        bindsym Return mode "default"
+        bindsym Escape mode "default"
+}
+
+bindsym $mod+r mode "resize"
+
+# Start i3bar to display a workspace bar (plus the system information i3status
+# finds out, if available)
+bar {
+        position top
+        status_command i3status
+        font termsyn:monospace 8
+}
+
+# exec dropbox start &
+# exec guake -name "guake-x" &
+# exec nm-applet &
+exec parcellite &
+exec dropbox &
+exec kitty &
+exec feh --randomize --bg-fill ~/Pictures/* &
+
+for_window [window_role="pop-up"] floating enable
+# for_window [title="Guake!"] floating enable
+# Gaps configuragion
+for_window [class=".*"] border pixel 1
+
+gaps inner 10
+gaps outer 10
+'';
+
+}
